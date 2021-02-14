be1bd99dfc1716ecf61b8c5ccf0531c740f3c1ae
GDWR <gregory.dwr@gmail.com>
2021-02-14 00:56:54 +0000

    Stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { FormsModule } from '@angular/forms';
+import { HomepageComponent } from './homepage/homepage.component';
+import { MAT_COLOR_FORMATS, NgxMatColorPickerModule, NGX_MAT_COLOR_FORMATS } from '@angular-material-components/color-picker';
+import { MatInputModule } from '@angular/material/input';
+import { FlexModule, FlexLayoutModule } from '@angular/flex-layout';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    NgxMatColorPickerModule,
+    MatInputModule,
+    FormsModule,
+    FlexModule,
+    FlexLayoutModule,
+    MatSelectModule,
+    MatSliderModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: MAT_COLOR_FORMATS, useValue: NGX_MAT_COLOR_FORMATS }
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,99 @@
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ThemePalette} from '@angular/material/core';
+import {AbstractControl, FormControl} from '@angular/forms';
+import {BehaviorSubject} from 'rxjs';
+
+class Colour {
+  alpha: number;
+  rgb: number[];
+  hex: string;
+
+  constructor(data: {a: number, b: number, g: number, hex: string, r: number, rgba: string, roundA: number}) {
+    this.alpha = data.a;
+    this.rgb = [data.r, data.g, data.b];
+    this.hex = '#' + data.hex;
+  }
+}
+
+interface Font {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements OnInit, AfterViewInit {
+
+  // Colours
+  bgSelectedColour: Colour = new Colour({a: 1, r: 255, g: 140, b: 0, hex: 'FF8C00', rgba: 'rgb(255, 140, 0)', roundA: 1});
+  fgSelectedColour: Colour = new Colour({a: 1, r: 18, g: 47, b: 121, hex: '122F79', rgba: 'rgb(18,47,121)', roundA: 1});
+  charSelectedColour: Colour = new Colour({a: 1, r: 253, g: 192, b: 36, hex: 'FDC024', rgba: 'rgb(253,173,36)', roundA: 1});
+
+  color: ThemePalette = 'primary';
+  colorCtr: AbstractControl = new FormControl(null);
+
+  // Other
+  character = 'G';
+  fontChoices: Font[] = [
+    {value: 'Helvetica', viewValue: 'Helvetica'},
+    {value: 'Roboto', viewValue: 'Roboto'},
+    {value: 'Arial', viewValue: 'Arial'},
+    {value: 'Verdana', viewValue: 'Verdana'},
+    {value: 'Goudy Bookletter 1911', viewValue: 'Goudy'},
+    {value: 'Georgia', viewValue: 'Gerogia'},
+    {value: 'Lucida Console', viewValue: 'Lucida'},
+    {value: 'Courier', viewValue: 'Courier'},
+    {value: 'Comic Sans MS', viewValue: 'Please Don\'t'},
+  ];
+  fontSelected = this.fontChoices[0].value;
+
+  // SVG
+  svgSize = 256;
+
+  @ViewChild('characterSvg') characterSvg: any;
+  textX = '49%';
+  textY = '55%';
+  aA = 10;
+  transformChar = `rotate(0, ${this.svgSize / 2}, ${this.svgSize / 2})`;
+  tempRotation = 0;
+  textSize = 128;
+  font: any;
+  textBoundingBox: [number, number] | undefined;
+
+  constructor() {
+  }
+
+
+  ngOnInit(): void {
+  }
+
+  selectBg(colour: any): void {
+    this.bgSelectedColour = new Colour(colour);
+  }
+
+  selectFg(colour: any): void {
+    this.fgSelectedColour = new Colour(colour);
+  }
+
+  selectCharColour(colour: any): void {
+    this.charSelectedColour = new Colour(colour);
+  }
+
+  calculateTextPos(): void {
+    console.log(this.character);
+  }
+
+  ngAfterViewInit(): void {
+    const bBox = this.characterSvg.nativeElement.getBBox();
+    this.textBoundingBox = [bBox.width, bBox.height];
+  }
+
+
+  valueChange(): void {
+    this.transformChar = `rotate(${this.tempRotation}, ${this.svgSize / 2}, ${this.svgSize / 2})`;
+  }
+
+}
